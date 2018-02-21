49d98c670bac676dbe1fc46d5fe7e10534591146
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-21 16:23:33 +0530

    c pgm
    good

---
+++ count.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char[1000],ch;
+int i;
+clrscr();
+
+   printf("Enter a character");
+   scanf("%c",&ch);
+
+   for(i = 0; ch[1000] = '\0'; ++i)
+   {
+       if(ch == ch[1000])
+           ++character;
+   }
+
+   printf("character of %c = %d", ch, character);
+   return 0;
+}
+
+
+
